587a93b5590b4dff71b47f40afbd78a422c0afb9
soso111borges <163007806+soso111borges@users.noreply.github.com>
2024-10-21 11:32:43 -0300

    .

--- main.js
+++ main.js
@@ -6,3 +6,34 @@
     const campoPalpite = document.querySelector('.campoPalpite');
     let contagemPalpites = 1;
     let botaoReiniciar;
+
+    function verificarPalpite() {
+      cost palpiteUsuario = Number(campoPalpite.value);
+      if(contagemPalpites ===1) {
+         palpites.textContent = "Palpites anteriores:  ";
+      }
+
+      palpites.textContent += palpitesUsuorio + "";
+
+      if(palpiteUsuario === numeroAleatorio) {
+         ultimoResultado.textContent = "Parabéns! Você acertou!";
+         ultimoResultado.style.backgroundColor = "green";
+         baixoOuAlto.textContent = "";
+         finalizarJogo();
+         } else if (contagemPalpites === 10) {
+          ultimoResultado.textContent = "FIM DE JOGO!!!";
+          baixoOuAlto.textContent = "";
+          finalizarJogo();
+   } else {
+      ultimoResultado.textContent = "Errado";
+      ultimoResultado.style.backgroundColor = "red";
+      if (palpitesUsuorio < numeroAleatorio) {
+      baixoOuAlto.textContent = "O último palpite foi muito baixo";
+   } else if (palpitesUsuorio > numeroAleatorio) {
+      baixoOuAlto.textContent = "O ultimo palpite foi muito alto";
+    }
+   }
+   
+
+
+         
